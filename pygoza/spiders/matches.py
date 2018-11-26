eb2f285b554b1e35b6fd79da9a31ce89a71f167b
Jaime Ibar <jim2k7@gmail.com>
2018-11-26 12:06:21 +0000

    Implemented matchloader in parse method.

--- pygoza/spiders/matches.py
+++ pygoza/spiders/matches.py
@@ -1,10 +1,9 @@
 import datetime
 
 import scrapy
-from scrapy.loader import ItemLoader
 from scrapy.loader.processors import Join
 
-from pygoza.items import ZaragozaMatchItem
+from pygoza.items import ZaragozaMatchItem, ZaragozaMatchItemLoader
 
 class MatchesSpider(scrapy.Spider):
 
@@ -16,13 +15,16 @@ class MatchesSpider(scrapy.Spider):
     def parse(self, response):
         matches = response.xpath('//div[starts-with(@class, "partido")]')
         for match in matches:
-            yield {
-                'date': self.get_match_day(match),
-                'time': self.get_match_time(match),
-                'localteam': match.xpath('.//span[@class="equipo local"]').xpath('.//span[@class="team"]/text()').extract_first(),
-                'foreignteam': match.xpath('.//span[@class="equipo visitante"]').xpath('.//span[@class="team"]/text()').extract_first(),
-                'finalscore': self.get_final_score(match)
-            }
+            matchloader = ZaragozaMatchItemLoader(ZaragozaMatchItem(), match)
+            matchloader.add_css('weekday', 'span.letra::text')
+            matchloader.add_css('day', 'span.dia::text')
+            matchloader.add_css('time', 'span.hora::text')
+            localteamloader = matchloader.nested_xpath('.//span[@class="equipo local"]')
+            localteamloader.add_xpath('localteam', './/span[@class="team"]/text()')
+            foreignteamloader = matchloader.nested_xpath('.//span[@class="equipo visitante"]')
+            foreignteamloader.add_xpath('foreignteam', './/span[@class="team"]/text()')
+            matchloader.add_xpath('finalscore', './/strong/text()')
+            yield matchloader.load_item()
 
     def get_final_score(self, match):
         """
@@ -33,16 +35,6 @@ class MatchesSpider(scrapy.Spider):
         fscoreprocessor = Join(' - ')
         fscore = fscoreprocessor(match.xpath('.//strong/text()').extract())
         return fscore
-
-    def get_match_day(self, match):
-        """
-        Extracts match day from match details.
-        :param match: Full match details.
-        :return: datetime.date object with hour and minutes.
-        """
-        year, month, day = map(int, match.css('span.hora::text').extract_first().strip(' ').split('-'))
-        matchday = datetime.date(year, month, day)
-        return matchday
 
     def get_match_time(self, match):
         """
