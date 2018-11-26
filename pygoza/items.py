78a820f14bca6e3c87ecf74909bda5040ba3d2bf
Jaime Ibar <jim2k7@gmail.com>
2018-11-26 12:07:05 +0000

    Implemented format_match_day function and ZaragozaMatchItemLoader class

--- pygoza/items.py
+++ pygoza/items.py
@@ -5,12 +5,40 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
+import datetime
+
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import Join, Compose, MapCompose, TakeFirst
+
+
+def format_match_day(day):
+    """
+    Converts string match day to datetime.date object
+    :param day: Unformatted match day
+    :return: datetime.date object year-month-day
+    """
+    day, month, year = map(int, day.split('-'))
+    matchday = datetime.date(year, month, day)
+    return matchday
 
 
 class ZaragozaMatchItem(scrapy.Item):
     weekday = scrapy.Field()
-    matchday = scrapy.Field()
-    matchtime = scrapy.Field()
+    day = scrapy.Field()
+    time = scrapy.Field()
     localteam = scrapy.Field()
     foreignteam = scrapy.Field()
+    finalscore = scrapy.Field()
+
+
+class ZaragozaMatchItemLoader(ItemLoader):
+    default_item_class = ZaragozaMatchItem
+    weekday_in = TakeFirst()
+    # day_in = MapCompose(str.strip, format_match_day)
+    """
+    time_in
+    localteam_in
+    foreignteam_in
+    finalscore_in
+    """
