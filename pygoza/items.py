0a0fa9d002cd7fc25454730ddb5e891e739fbab9
Jaime Ibar <jim2k7@gmail.com>
2018-11-27 17:08:05 +0000

    Added loggin system. Implemented get_match_time function to get match time. Implemented weekday_in, day_in and time_in items.

--- pygoza/items.py
+++ pygoza/items.py
@@ -6,10 +6,14 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import datetime
+import logging
 
 import scrapy
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import Join, Compose, MapCompose, TakeFirst
+from scrapy.loader.processors import Compose, MapCompose, Identity
+
+
+logger = logging.getLogger(__name__)
 
 
 def format_match_day(day):
@@ -22,6 +26,18 @@ def format_match_day(day):
     matchday = datetime.date(year, month, day)
     return matchday
 
+def get_match_time(mtime):
+    """
+    Converts string time into datetime.time object
+    :param time: Match time details.
+    :return: datetime.time object with hour and minutes.
+    """
+    if not mtime or mtime == ' ':
+        return 'N/A'
+    for tme in mtime:
+        hour, minutes = map(int, tme.strip(' ·').split(':'))
+        matchtime = datetime.time(hour, minutes)
+        return matchtime
 
 class ZaragozaMatchItem(scrapy.Item):
     weekday = scrapy.Field()
@@ -34,10 +50,10 @@ class ZaragozaMatchItem(scrapy.Item):
 
 class ZaragozaMatchItemLoader(ItemLoader):
     default_item_class = ZaragozaMatchItem
-    weekday_in = TakeFirst()
-    # day_in = MapCompose(str.strip, format_match_day)
+    weekday_in = Identity()
+    day_in = MapCompose(str.strip, format_match_day)
+    time_in = Compose(get_match_time)
     """
-    time_in
     localteam_in
     foreignteam_in
     finalscore_in
