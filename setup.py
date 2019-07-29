aa0ca0f6ff7ac6f12948680b44616dcaf5f2544c
Jaime Ibar <jim2k7@gmail.com>
2019-07-29 19:23:51 +0100

    Added Python 3.4 and Python 3.5 to supported versions.

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Utilities'
